5176a806dc9134d5ceb991a136fadca224884051
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-28 16:50:19 +0200

    Add 'render' functionality and style

--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/index.css';
 
 class Game extends React.Component {
     constructor(props) {
@@ -18,10 +19,21 @@ class Game extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="game-wrapper">
                 <h1>Welcome to TicTacToe game!</h1>
+                {this.state.winner ? <h3>{`And the winner is ${this.state.winner}`}</h3> : null}
+                <div className="board">
+                    {this.state.board.map((cell, index) => {
+                        return <div onClick={() => this.handleClick(index)} className="square" key={index}>{cell}</div>
+                    })}
+                </div>
             </div>
         );
+    }
+
+    //custom functions
+    handleClick(index){
+
     }
 }
 
